02c5b21ee6fe486105e4d072c2f0e313f77c8481
Pedro Morales <pedrom2@csu.fullerton.edu>
2022-11-27 11:09:18 -0800

    fixed tailwind css loading error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['*.{html,js}'],
+  content: [
+    './assets/**/*.{html, js}',
+    './index.html'
+  ],
   theme: {
     extend: {}
   },
